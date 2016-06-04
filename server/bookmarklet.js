1b1a72dde9f204266daa2678b73425acffc25fe4
David Villegas <dville00@gmail.com>
2016-06-04 17:06:05 -0400

    annotations fixes

--- server/bookmarklet.js
+++ server/bookmarklet.js
@@ -1,17 +1,47 @@
 (function() {
 
-  var selectedElement = "Some text";
+  var selectedElements = [];
 
+
+  var cannSubmitShow = function() {
+
+    console.log("show!");
+    $("#cann-submit-dialog").show();
+  }
 
   var cannSubmit = function() {
 
+    console.log("SUBMIT");
+
+    var elementsStr = "";
+
+    if (selectedElements.length > 0) {
+      var element = selectedElements[0];
+      var elementType  = element.get(0).tagName;
+      var elementId = element.prop('id');
+      var elementClass = element.attr('class');
+
+      console.log(element);
+      console.log(element.get(0).id);
+      console.log(element.id);
+      console.log(element.attr('id'));
+
+      elementStr = "<" + elementType + " id=" + elementId + " class=" + elementClass + " >";
+    }
+
+
+
     var annotationData = {
       name: "test",
-      element: selectedElement,
+      element: elementStr,
+      timestamp: new Date(),
+      user: "Anonymous User",
+      url: window.location.toString(),
+      comments: $("#cann-comments").val(),
     };
 
     $.ajax({
-      url: "http://localhost:5000/hello",
+      url: "http://localhost:5000/hello/",
       type: "POST",
       data: annotationData,
       success: function(data, status, jqXHR) {
@@ -28,12 +58,21 @@
       }
     });
 
+    var i;
+    for(i = 0; i < selectedElements.length; i++) {
+      selectedElements[i].removeClass('cann-highlight');
+    }
+
+    $("#cann-submit-dialog").hide();
+    selectedElements = [];
+
   };
 
   var cannAnnotateHandler = function(e) {
 
     var target = $(e.target);
     target.addClass('cann-highlight');
+    selectedElements.push(target);
     this.removeEventListener('click', cannAnnotateHandler);
 
   };
@@ -52,13 +91,24 @@
 
     var menu = $("<div id='cann-menu'></div>");
     var annotateBtn = $("<a href='javascript:cannAnnotate()'>Annotate</a>");
-    var submitBtn = $("<a href='javascript:cannSubmit()'>Submit</a>");
+    var submitBtn = $("<a href='javascript:cannSubmitShow()'>Submit</a>");
 
     menu.append(annotateBtn);
     menu.append(submitBtn);
 
     $('body').append(menu);
 
+    // Submission dialog
+    var submitDialog = $("<div id='cann-submit-dialog'>Please enter your comments</div>");
+    submitDialog.append($("<textarea id='cann-comments' rows='14' cols='40'></textarea>"));
+
+    var submitButton = $("<button id='cann-submit-button'>Submit</button>");
+    submitButton.click(cannSubmit);
+
+    submitDialog.append(submitButton);
+
+
+    $('body').append(submitDialog);
 
   };
 
@@ -66,5 +116,7 @@
   showMenu();
 
   window.cannAnnotate = cannAnnotate;
+  window.cannSubmit = cannSubmit;
+  window.cannSubmitShow = cannSubmitShow;
 
 })();
